d97408c44ad0c3eafac49969a52ab1a41adc914e
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-03-13 11:14:21 +0200

    Added tokenUtils

---
+++ server/src/utils/tokenUtil.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import 'dotenv/config'
+
+export function generateAuthToken(user) {
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '15m' });
+
+    return token;
+}
+
+export function generateRefeshToken(user) {
+    const payload = {
+        id: user.id,
+        email: user.email
+    }
+
+    const token = jwt.sign(payload, process.env.REFRESH_JWT_SECRET, { expiresIn: '14d' });
+
+    return token;
+}
