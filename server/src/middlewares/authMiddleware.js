48926f73633b50afb6e778471c9f5dd1c3c4f1d6
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-03-13 11:14:47 +0200

    Added authMiddleware

---
+++ server/src/middlewares/authMiddleware.js
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+import jwt from 'jsonwebtoken'
+
+export default function authMiddleware(req, res, next) {
+    const token = req.header('X-Authorization');
+
+    if (!token) {
+        next();
+    }
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decodedToken;
+        req.isAuthenticated = true;
+
+        next()
+    } catch (error) {
+        res.status(401).end();
+    }
+}
+
+export function isAuth(req, res, next) {
+    if (!req.isAuthenticated) {
+        return res.status(401);
+    }
+
+    next();
+}
+
+export function isGuest(req, res, next) {
+    if (req.isAuthenticated) {
+        return res.status(403);
+    }
+
+    next();
+}
