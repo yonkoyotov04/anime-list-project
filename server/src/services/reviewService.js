f384b46501782b07bb5512b68ae3f3b599048170
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-03-13 12:25:18 +0200

    Added Review Service

---
+++ server/src/services/reviewService.js
@@ -0,0 +1,35 @@
+import Review from "../models/Review.js"
+
+export default {
+    reviewAnime(reviewData) {
+        return Review.create(reviewData);
+    },
+
+    getReviewById(reviewId) {
+        return Review.findById(reviewId);
+    },
+
+    getAnimeReviews(animeId) {
+        return Review.find({anime: animeId}).populate({path: 'user', select: 'username profilePic'});
+    },
+
+    getUserReviews(userId) {
+        return Review.find({user: userId}).populate({path: 'anime', select: 'title imageUrl'})
+    },
+
+    editReview(reviewId, newReview) {
+        return Review.findByIdAndUpdate(reviewId, newReview, {runValidators: true, new: true});
+    },
+
+    deleteReview(reviewId) {
+        return Review.findByIdAndDelete(reviewId);
+    },
+
+    deleteReviewsForAnime(animeId) {
+        return Review.deleteMany({anime: animeId});
+    },
+
+    deleteReviewsForUser(userId) {
+        return Review.deleteMany({user: userId});
+    }
+}
