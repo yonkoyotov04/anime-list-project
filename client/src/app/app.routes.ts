e2b82a9374adc9eb50fa2518332e8edc94c024cc
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-03-20 10:49:06 +0200

    Added a profile component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
 import { CatalogueComponent } from './features/catalogue/catalogue.component';
 import { AboutComponent } from './features/about/about.component';
+import { ProfileComponent } from './features/profile/profile.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'register', component: RegisterComponent},
     {path: 'login', component: LoginComponent},
     {path: 'catalogue', component: CatalogueComponent},
-    {path: 'about', component: AboutComponent}
+    {path: 'about', component: AboutComponent},
+    {path: 'profile', component: ProfileComponent}
 ];
