ffa0473ec66a7c4ff893bd11f05c76e58f66d6dd
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-03-20 10:39:31 +0200

    Added a catalogue component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './features/register/register.component';
 import { LoginComponent } from './features/login/login.component';
 import { HomeComponent } from './features/home/home.component';
+import { CatalogueComponent } from './features/catalogue/catalogue.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'register', component: RegisterComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'catalogue', component: CatalogueComponent}
 ];
---
+++ client/src/app/features/catalogue/catalogue.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue',
+  imports: [],
+  templateUrl: './catalogue.component.html',
+  styleUrl: './catalogue.component.css',
+})
+export class CatalogueComponent {}
