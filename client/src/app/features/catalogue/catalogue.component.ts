26d6845f454b87e82f7c6a048c8802f0dea2d8ae
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-04-06 11:57:21 +0300

    Sorting for catalogue works
    Animes in the catalogue can now be sorted by a few different criteria

--- client/src/app/features/catalogue/catalogue.component.ts
+++ client/src/app/features/catalogue/catalogue.component.ts
@@ -1,17 +1,48 @@
-import { Component, OnInit, signal } from '@angular/core';
-import { RouterLink } from "@angular/router";
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
 import { Api } from '../../core/services/api.service';
 import { Anime } from '../../shared/interfaces/anime';
 import { AnimeItemComponent } from '../../shared/components/anime-item/anime-item.component';
+import { FormBuilder, ReactiveFormsModule, Validators, ɵInternalFormsSharedModule } from '@angular/forms';
 
 @Component({
     selector: 'app-catalogue',
-    imports: [AnimeItemComponent],
+    imports: [AnimeItemComponent, ReactiveFormsModule],
     templateUrl: './catalogue.component.html',
     styleUrl: './catalogue.component.css',
 })
 export class CatalogueComponent implements OnInit {
+    private formBuilder = inject(FormBuilder);
+
     animes = signal<Anime[]>([]);
+    sortType = signal<string | null>(null);
+
+    sortedAnimes = computed(() => {
+        const list = this.animes();
+        const type = this.sortType();
+        
+        switch(type) {
+            case 'title(alph)': {
+                return [...list].sort((a, b) => a.title.localeCompare(b.title));
+            }
+            case 'title(reAlph)': {
+                return [...list].sort((a, b) => b.title.localeCompare(a.title));
+            }
+            case 'rating(asc)': {
+                return [...list].sort((a, b) => a.rating - b.rating);
+            }
+            case 'rating(desc)': {
+                return [...list].sort((a, b) => b.rating - a.rating);
+            }
+            case 'newest': {
+                return [...list].sort((a, b) => new Date(b.startDate).getTime() - new Date(a.startDate).getTime());
+            }
+            case 'oldest': {
+                return [...list].sort((a, b) => new Date(a.startDate).getTime() - new Date(b.startDate).getTime());
+            }
+        }
+
+        return list;
+    })
 
     constructor(private apiService: Api) { }
 
@@ -19,5 +50,19 @@ export class CatalogueComponent implements OnInit {
         this.apiService.getAnime().subscribe((animes) => {
             this.animes.set(animes);
         })
+    }
+
+    searchForm = this.formBuilder.group({
+        type: ['title', [Validators.required]],
+        search: ['']
+    })
+
+    sortForm = this.formBuilder.group({
+        criteria: ['title(alph)', [Validators.required]]
+    })
+
+    onSort():void {
+        const sortValue = this.sortForm.value.criteria;
+        this.sortType.set(sortValue!);        
     }
 }
