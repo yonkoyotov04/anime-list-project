a10b0a6926813f65daebc4311b5e2a117caa4d5c
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-04-13 10:19:17 +0300

    Changed how the profile link looks in header

--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { Router, RouterLink } from "@angular/router";
 import { Auth } from '../../core/services/auth.service';
 import { ErrorMessageComponent } from '../../shared/components/error-message/error-message.component';
@@ -10,21 +10,32 @@ import { Notif } from '../../core/services/notif.service';
     templateUrl: './header.component.html',
     styleUrl: './header.component.css',
 })
-export class Header {
+export class Header implements OnInit {
     private notifService = inject(Notif);
 
     notification = this.notifService.notification;
 
+    // userData = signal<any>(null)
+
     constructor(private authService: Auth, private router: Router) { }
 
-    isAuthenticated() {
+    ngOnInit(): void {
+        // this.userData.set({ username: this.authService.user().username, profilePic: this.authService.user().profilePic });
+    }
+
+    isAuthenticated(): any {
         return this.authService.isAuthenticated();
+    }
+
+    userAvatar(): any {
+        return { username: this.authService.user().username, profilePic: this.authService.user().profilePic }
     }
 
     logout() {
         this.authService.logout().subscribe({
             next: () => {
                 this.authService.afterLogout();
+                // this.userData.set(null);
                 this.router.navigateByUrl('/');
             }
         });
