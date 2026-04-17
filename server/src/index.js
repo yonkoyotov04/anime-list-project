80765ab50b7e1a3246786ce496af66e0f4ca9ba2
yonkoyotov04 <yonkoyotov04@gmail.com>
2026-04-17 11:55:59 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -25,8 +25,6 @@ app.use(cors({
     allowedHeaders: ['Content-Type', 'X-Authorization', 'isAuth']
 }));
 
-app.options("/*", cors())
-
 app.use(express.json());
 app.use(cookieParser());
 app.use(authMiddleware);
